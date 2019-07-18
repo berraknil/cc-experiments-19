9a006c5044343cf24c4498ac6b6a47e14cd62ce4
Berrak <Phoenix@r00t.local>
2019-07-18 13:12:03 +0200

    different print settings

--- sketch.js
+++ sketch.js
@@ -1,20 +1,24 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [2048, 2048],
+  // orientation: 'landscape',
+  // units: 'cm',
+  // pixelsPerInch: 300,
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
+    console.log(width, height);
     context.fillStyle = 'red';
     context.fillRect(0, 0, width, height);
 
     context.beginPath();
-    context.arc(width/2, height/2, 300, Math.PI * 2, false);
-    context.fillStyle = "orange";
+    context.arc(width / 2, height / 2, width * 0.2, Math.PI * 2, false);
+    context.fillStyle = 'orange';
     context.fill();
-    context.lineWidth = 30;
-    context.strokeStyle = "blue";
+    context.lineWidth = width * 0.05;
+    context.strokeStyle = 'blue';
     context.stroke();
   };
 };
